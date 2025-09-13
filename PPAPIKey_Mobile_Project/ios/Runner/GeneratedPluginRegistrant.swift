eabe469458d9db3e08ae8be2bf809ab43f4439d0
Hoàng Minh Hiếu <141487551+Nidios1@users.noreply.github.com>
2025-09-14 01:09:55 +0700

    Add files via upload

---
+++ PPAPIKey_Mobile_Project/ios/Runner/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import Flutter
+import UIKit
+
+@objc class GeneratedPluginRegistrant: NSObject {
+  @objc static func register(with registry: FlutterPluginRegistry) {
+    // No plugins to register for now
+  }
+}
